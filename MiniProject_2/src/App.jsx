60727797d3c3149127e7c591cf96c0b0dae662bd
Samiran Bisal <samiranbisal@Samirans-MacBook-Air.local>
2023-12-03 12:27:30 +0530

    fews

--- MiniProject_2/src/App.jsx
+++ MiniProject_2/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='p-4 text-7xl text-blue-700 bg-pink-600 font-bold'>Project 2</h1>
+      <h1 className="p-4 text-7xl text-blue-700 bg-pink-600 font-bold">
+        Project 2
+      </h1>
+
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {}
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {}
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
