5f175fdf4198b10b03cf5b5540aeef480802e8af
Samiran Bisal <samiranbisal@Samirans-MacBook-Air.local>
2023-12-03 19:20:20 +0530

    code mistack

--- MiniProject_2/src/App.jsx
+++ MiniProject_2/src/App.jsx
@@ -1,26 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./contexts/Theme";
+import Card from "./components/Card";
+import Thememore from "./components/Thememore";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [ThemeMode, setThemeMore] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMore("light")
+  }
+  const darkTheme = () => {
+    setThemeMore('dark')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(ThemeMode)
+  },[ThemeMode])
 
   return (
     <>
-      <h1 className="p-4 text-7xl text-blue-700 bg-pink-600 font-bold">
+      <h1 className="p-4 text-7xl text-blue-700 bg-pink-600 font-bold" >
         Project 2
       </h1>
+      <ThemeProvider value={{ThemeMode, lightTheme, darkTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <Thememore/>
+            </div>
 
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {}
-          </div>
-
-          <div className="w-full max-w-sm mx-auto">
-            {}
+            <div className="w-full max-w-sm mx-auto">
+              <Card/>
+            </div>
           </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
